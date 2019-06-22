48af813ba2f69ae9a85feb218248ed126c929ed4
jamday <jgascoignetaylor@godaddy.com>
2019-06-22 14:40:24 +0200

    add eps

--- app.rb
+++ app.rb
@@ -1,8 +1,15 @@
 require 'sinatra'
-set :bind, '0.0.0.0'
-
+require 'eps'
 class HelloWorldApp < Sinatra::Base
   get '/' do
     "Hello, world!"
+    data = [
+      {bedrooms: 1, bathrooms: 1, price: 100000},
+      {bedrooms: 2, bathrooms: 1, price: 125000},
+      {bedrooms: 2, bathrooms: 2, price: 135000},
+      {bedrooms: 3, bathrooms: 2, price: 162000}
+    ]
+    model = Eps::Model.new(data, target: :price)
+    puts model.summary
   end
 end
